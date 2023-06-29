00398fe487cf176aeaabcc60a05db1f0de5be21e
root <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@github.com>
2023-06-29 11:03:21 +0000

    fix some bugs on generate caption

--- gen_precise_cap.py
+++ gen_precise_cap.py
@@ -32,19 +32,28 @@ def run_caption(args, img_name, img_pil_list, init_caption, lm_model, lm_tokeniz
             generate_order = args.order
           )
 
-
-    for iter_id, gen_text_list in enumerate(gen_texts):
+    """for iter_id, gen_text_list in enumerate(gen_texts):
         # for jj in range(len(gen_text_list)):
         for jj in [len(gen_text_list) - 1]:
             image_id = img_name[0] # img_name[jj].split(".")[0]
             gen_text_list[jj] = gen_text_list[jj].replace('image of ', '').capitalize()
-
+            print("fix bug", all_results)
             if all_results[iter_id]==None:
                 # all_results[iter_id] = {image_id: gen_text_list[jj]}
                 all_results[iter_id] = []
             else:
                 all_results[iter_id].append({"image_id": image_id,
-                                         "caption": gen_text_list[jj]})
+                                         "caption": gen_text_list[jj]})"""
+    
+    # for jj in range(len(gen_text_list)):
+    # for jj in [len(gen_text_list) - 1]:
+    jj = len(gen_texts[0]) - 2
+    image_id = img_name[0] # img_name[jj].split(".")[0]
+    gen_texts[0][jj] = gen_texts[0][jj].replace('image of ', '').capitalize()
+        
+    all_results.append({"image_id": image_id,
+                        "caption": gen_texts[0][jj]})
+                            
 
     return all_results
 
@@ -52,21 +61,19 @@ def run_caption(args, img_name, img_pil_list, init_caption, lm_model, lm_tokeniz
 def gen_coco_cap_json(
     args, run_type, 
     lm_model, lm_tokenizer, clip, token_mask, logger,
-    train_loader,
+    train_loader, 
 ):
     if args.run_type == 'caption' and args.order == 'precise':
         for sample_id in range(args.samples_num):
-            all_results = [None] * (args.num_iterations+1)
+            all_results = [] # [None] * (args.num_iterations+1)
             logger.info(f"Sample {sample_id+1}: ")
             for batch_idx, (img_batch_pil_list, name_batch_list, init_caption_list) in enumerate(train_loader):
                 logger.info(f"The {batch_idx+1}-th batch:")
-                if args.run_type == 'caption':
-                    all_results = run_caption(
-                        args, name_batch_list, img_batch_pil_list,
-                        init_caption_list,
-                        lm_model,
-                        lm_tokenizer, clip, token_mask, logger, all_results)
-            
+                all_results = run_caption(
+                    args, name_batch_list, img_batch_pil_list,
+                    init_caption_list,
+                    lm_model,
+                    lm_tokenizer, clip, token_mask, logger, all_results)
             
                 save_dir = "results/caption_%s_len%d_topk%d_alpha%.3f_beta%.3f_gamma%.3f_theta%.3f_lmTemp%.3f/sample_%d" % (
                     args.order,args.sentence_len, args.candidate_k, args.alpha, args.beta,args.gamma,args.theta,args.lm_temperature,sample_id
