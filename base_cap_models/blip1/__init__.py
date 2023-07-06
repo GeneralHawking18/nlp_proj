48e73ceafe6339d068e783da624d1ad77c2ec031
generalhawking18 <chunghoang0103@gmail.com>
2023-07-06 03:47:52 +0000

    add gen_base_cap features

--- base_cap_models/blip1/__init__.py
+++ base_cap_models/blip1/__init__.py
@@ -1,27 +1,32 @@
-from lavis.models import load_model_and_preprocess
+from transformers import AutoProcessor, BlipForConditionalGeneration
 import torch
 import torch.nn as nn
 
-class BLIP(nn.Module):
+class BLIP_1(nn.Module):
     def __init__(self, mode = "eval"):
         super().__init__()
         self.device = device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.mode = mode 
-        self.model, self.vis_processors, _ = load_model_and_preprocess(
-            name="blip_caption", 
-            model_type="base_coco", 
-            is_eval = (mode == "eval"),
-            device=self.device
-        )
-        self.change_mode(mode)
+        name_model = "Salesforce/blip-image-captioning-base"
+        self.processors = AutoProcessor.from_pretrained(name_model)
+        self.model = BlipForConditionalGeneration.from_pretrained(name_model)
+        self.init_prompt = 'A photo of'
 
-    def change_mode(self, mode = "eval"):
-        self.transforms = self.vis_processors[mode]
+    def set_init_prompt(self, prompt):
+        self.init_prompt = prompt
     
     def get_transforms(self):
-        return self.transforms
+        return self.processors
 
     def forward(self, image):
-        caption = self.model.generate({"image": image})
-        return caption
+        inputs = self.processors(images=image, text = self.init_prompt, return_tensors="pt")
+        outs = self.model.generate(**inputs)
+        captions = self.processors.decode(outs[0], skip_special_tokens=True)
+        return self.remove_redundances(captions)
     
+    def remove_redundances(self, captions):
+        if isinstance(captions, list):
+            for i in range(len(captions)):
+                captions[i] = captions[i].replace(self.init_prompt.lower(), '')
+        else:
+            captions = captions.replace(self.init_prompt.lower(), '')
+        return captions
